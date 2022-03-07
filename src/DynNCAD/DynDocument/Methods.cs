ebca856448d56c65b17efb424a2dac0544c906f5
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-07 23:02:58 +0300

    Create Methods.cs
    Empty class with document methods (no working ..)
    may be wrong API?

---
+++ src/DynNCAD/DynDocument/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static nanoCAD.Init;
+
+namespace nanoCAD.DynDocument
+{
+    public class Methods
+    {
+
+    }
+}
