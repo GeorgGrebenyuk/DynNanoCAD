b42dd45f5c2eadecdd26a93cb4eb2ea92771f103
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-07 21:02:52 +0300

    Create Init.cs
    Root class that consicts of NC_Application object

---
+++ src/DynNCAD/Init.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nanoCAD;
+
+namespace DynNCAD
+{
+
+    public class Init
+    {
+        private Init (){}
+        public static nanoCAD.Application inc_app;
+        public static nanoCAD.Document inc_doc;
+    }
+}
