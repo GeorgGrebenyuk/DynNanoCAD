7d96e9012f724f9dd0fbefb525806af002f1beb1
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-07 23:02:01 +0300

    Update Init.cs
    Cnahge core class -- add initiation methods

--- src/DynNCAD/Init.cs
+++ src/DynNCAD/Init.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using nanoCAD;
 
-namespace DynNCAD
+namespace nanoCAD
 {
-
     public class Init
     {
         private Init (){}
         public static nanoCAD.Application inc_app;
         public static nanoCAD.Document inc_doc;
+        public static OdaX.IAcadDatabase inc_db;
+        private static void InitApp()
+        {
+            inc_app = System.Runtime.InteropServices.Marshal.GetActiveObject("nanoCAD.Application") as nanoCAD.Application;
+        }
+        public static int CurrentDoc()
+        {
+            inc_app = System.Runtime.InteropServices.Marshal.GetActiveObject("nanoCAD.Application") as nanoCAD.Application;
+            inc_doc = inc_app.ActiveDocument;
+            inc_db = inc_doc.Database;
+
+            return 1;
+        }
+        public static string AboutPackage()
+        {
+            return $@"Look package's site https://github.com/GeorgGrebenyuk/DynNanoCAD/tree/main/src/DynNCAD";
+        }
+        public static string GetDrawingName(int aux)
+        {
+            nanoCAD.Document doc2 = inc_doc as nanoCAD.Document;
+            return doc2.FullName;
+        }
     }
 }
