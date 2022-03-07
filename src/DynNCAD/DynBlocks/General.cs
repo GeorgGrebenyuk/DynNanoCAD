b71744de948e861ec28afa61eb357a6ac713f4b0
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-03-07 23:52:19 +0300

    Create General.cs
    base method to getting all document's blocks

---
+++ src/DynNCAD/DynBlocks/General.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using static nanoCAD.Init;
+using OdaX;
+
+namespace nanoCAD.DynBlocks
+{
+    public class General
+    {
+        private General() { }
+        public static List <object> GetAllBlocks (int aux)
+        {
+            IAcadBlocks coll = inc_doc.Blocks;
+            List<object> blocks = new List<object>();
+            for (int i1 = 0; i1 < coll.Count; i1++)
+            {
+                blocks.Add(coll.Item(i1));  
+            }
+            return blocks;
+        }
+    }
+}
