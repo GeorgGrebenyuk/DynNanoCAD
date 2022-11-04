93258dd7b2dd77e8c304a8af9f24a5e5ce96c98e
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-11-04 22:50:47 +0300

    Create dyn_template.cs
    Шаблон для создания новых классов

---
+++ src/DynNCAD/dyn_template.cs
@@ -0,0 +1,37 @@
+﻿#region include_namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dr = Autodesk.DesignScript.Runtime;
+using dg = Autodesk.DesignScript.Geometry;
+
+using nanoCAD;
+using OdaX;
+#endregion
+
+namespace DynNCAD
+{
+    [dr.IsVisibleInDynamoLibrary(false)]
+    public class dyn_template
+    {
+        public OdaX.AcadEntity _i;
+        #region constructors
+        internal dyn_template() { }
+        public dyn_template (dynamic entity)
+        {
+            this._i = entity._i;
+        }
+        #endregion
+
+
+        #region functions
+        #endregion
+
+
+        #region properties
+        #endregion
+    }
+}
